b1baca60dfd0a8f895f5f39f262a664ec2726d44
jgomez <jgomez@spyrohost.com>
2019-11-10 14:12:33 -0500

    cambios local

---
+++ index.php
@@ -0,0 +1,267 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<title>Empresas Perú</title>
+	<meta name="robots" content="NOINDEX">
+	<link rel="stylesheet" href="css/bootstrap.css">
+	<link rel="stylesheet" href="css/fontawesome.css">
+	<link href="css/brands.css" rel="stylesheet">
+  	<link href="css/solid.css" rel="stylesheet">
+	<link rel="stylesheet" href="css/style.css">
+</head>
+<body>
+<header>
+	<div class="container">
+		<div class="row">
+			<div class="col-md-4">
+				<div class="logo">
+					<img src="img/logo.png">
+				</div>
+			</div>
+			<div class="col-md-8">
+				<nav class="menu">
+					<ul>
+						<li><a href="javascript:void(0)">Inicio</a></li>
+						<li><a href="javascript:void(0)">Empresas</a></li>
+						<li><a href="javascript:void(0)">Integración</a></li>
+						<li><a href="javascript:void(0)">Api</a></li>
+						<li><a href="javascript:void(0)">Planes</a></li>
+						<li><a href="javascript:void(0)">Contacto</a></li>
+						<li><a href="javascript:void(0)" class="sp-btn sp-btn-blue">Ingresar</a></li>
+						<li><a href="javascript:void(0)" class="sp-btn sp-btn-light-blue">Registro</a></li>
+					</ul>
+				</nav>
+			</div>
+		</div>
+	</div>
+</header>
+<section class="banner">
+	<div class="container">
+		<div class="row">
+			<div class="col-md-8">
+				<div class="informacion">
+					<h1 class="h1">Información Empresarial en Línea</h1>
+					<p>Datos de más de 1 Millón de empresas en tiempo real</p>
+					<p>Información Sucursales</p>
+					<p>Representantes Legales</p>
+					<p>Record de número de trabajadores</p>
+					<a href="javascript:void(0)" class="sp-btn sp-btn-light-blue sp-btn-shadow">Ver Planes</a>
+				</div>
+			</div>
+			<div class="col-md-4">
+				<div class="imagen">
+					<img src="img/informacion.png">
+				</div>
+			</div>
+		</div>
+	</div>
+</section>
+<section class="busqueda-empresas">
+	<div class="container">
+		<div class="row">
+			<div class="col-md-12">
+				<h1 class="h1 h1-border-bottom">Búsqueda de Empresas</h1>
+				<div class="input-group">
+					<input type="text" placeholder="Número de RUC o Razón Social">
+					<button type="button" class="sp-btn sp-btn-light-blue">Buscar</button>
+				</div>
+			</div>
+		</div>
+	</div>
+</section>
+<section class="rubros-importantes">
+	<div class="container">
+		<div class="row">
+			<div class="col-md-12">
+				<h1 class="h1 h1-border-bottom">Rubros más Importantes</h1>
+			</div>
+		</div>
+		<div class="row">
+			<div class="col-md-12">
+				<div class="rubros">
+					<div class="bloque">
+						<div class="titulo">Agricolas</div>
+						<div class="cantidad">+ 3500</div>
+					</div>
+					<div class="bloque">
+						<div class="titulo">Agricolas</div>
+						<div class="cantidad">+ 3500</div>
+					</div>
+					<div class="bloque">
+						<div class="titulo">Agricolas</div>
+						<div class="cantidad">+ 3500</div>
+					</div>
+					<div class="bloque">
+						<div class="titulo">Agricolas</div>
+						<div class="cantidad">+ 3500</div>
+					</div>
+					<div class="bloque">
+						<div class="titulo">Agricolas</div>
+						<div class="cantidad">+ 3500</div>
+					</div>
+					<div class="bloque">
+						<div class="titulo">Agricolas</div>
+						<div class="cantidad">+ 3500</div>
+					</div>
+					<div class="bloque">
+						<div class="titulo">Agricolas</div>
+						<div class="cantidad">+ 3500</div>
+					</div>
+					<div class="bloque">
+						<div class="titulo">Agricolas</div>
+						<div class="cantidad">+ 3500</div>
+					</div>
+				</div>
+			</div>
+		</div>
+	</div>
+</section>
+<section class="empresas-importantes contenedor-empresas">
+	<div class="container">
+		<div class="row">
+			<div class="col-md-12">
+				<h1 class="h1 h1-border-bottom">Empresas privadas más Importantes</h1>
+			</div>
+		</div>
+		<div class="row">
+			<div class="col-md-12">
+				<div class="empresas">
+					<div class="bloque">
+						<div class="fondo">
+							<div class="icono">
+								<i class="fas fa-plus-circle"></i>
+							</div>
+						</div>
+						<div class="encabezado">Banco de Credito del Perú</div>
+					</div>
+					<div class="bloque">
+						<div class="fondo">
+							<div class="icono">
+								<i class="fas fa-plus-circle"></i>
+							</div>
+						</div>
+						<div class="encabezado">Banco de Credito del Perú</div>
+					</div>
+					<div class="bloque">
+						<div class="fondo">
+							<div class="icono">
+								<i class="fas fa-plus-circle"></i>
+							</div>
+						</div>
+						<div class="encabezado">Banco de Credito del Perú</div>
+					</div>
+					<div class="bloque">
+						<div class="fondo">
+							<div class="icono">
+								<i class="fas fa-plus-circle"></i>
+							</div>
+						</div>
+						<div class="encabezado">Banco de Credito del Perú</div>
+					</div>
+					<div class="bloque">
+						<div class="fondo">
+							<div class="icono">
+								<i class="fas fa-plus-circle"></i>
+							</div>
+						</div>
+						<div class="encabezado">Banco de Credito del Perú</div>
+					</div>
+				</div>
+			</div>
+		</div>
+		<div class="row">
+			<div class="col-md-12">
+				<div class="boton">
+					<a href="javascript:void(0)" class="sp-btn sp-btn-blue">Empresas con mayor crecimiento</a>
+				</div>
+			</div>
+		</div>
+	</div>
+</section>
+<section class="empresas-crecimiento-laboral contenedor-empresas">
+	<div class="container">
+		<div class="row">
+			<div class="col-md-12">
+				<h1 class="h1 h1-border-bottom">Empresas con mayor crecimiento Laboral</h1>
+			</div>
+		</div>
+		<div class="row">
+			<div class="col-md-12">
+				<div class="empresas">
+					<div class="bloque">
+						<div class="fondo">
+							<div class="icono">
+								<i class="fas fa-plus-circle"></i>
+							</div>
+						</div>
+						<div class="encabezado">Banco de Credito del Perú</div>
+					</div>
+					<div class="bloque">
+						<div class="fondo">
+							<div class="icono">
+								<i class="fas fa-plus-circle"></i>
+							</div>
+						</div>
+						<div class="encabezado">Banco de Credito del Perú</div>
+					</div>
+					<div class="bloque">
+						<div class="fondo">
+							<div class="icono">
+								<i class="fas fa-plus-circle"></i>
+							</div>
+						</div>
+						<div class="encabezado">Banco de Credito del Perú</div>
+					</div>
+					<div class="bloque">
+						<div class="fondo">
+							<div class="icono">
+								<i class="fas fa-plus-circle"></i>
+							</div>
+						</div>
+						<div class="encabezado">Banco de Credito del Perú</div>
+					</div>
+					<div class="bloque">
+						<div class="fondo">
+							<div class="icono">
+								<i class="fas fa-plus-circle"></i>
+							</div>
+						</div>
+						<div class="encabezado">Banco de Credito del Perú</div>
+					</div>
+				</div>
+			</div>
+		</div>
+		<div class="row">
+			<div class="col-md-12">
+				<div class="boton">
+					<a href="javascript:void(0)" class="sp-btn sp-btn-blue">Empresas con mayor crecimiento</a>
+				</div>
+			</div>
+		</div>
+	</div>
+</section>
+<footer>
+	<div class="container">
+		<div class="row">
+			<div class="col-md-4">
+				<div class="logo">
+					<img src="img/f_logo.png">
+				</div>
+			</div>
+			<div class="col-md-8">
+				<nav class="menu">
+					<ul>
+						<li>Términos y Condiciones</li>
+						<li>Contáctenos</li>
+					</ul>
+				</nav>
+			</div>
+		</div>
+	</div>
+</footer>
+<script src="js/jquery.min.js"></script>
+<script src="js/bootstrap.js"></script>
+<script src="js/fontawesome.js"></script>
+</body>
+</html>
